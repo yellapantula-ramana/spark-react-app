7572c89343126b56929dde82cf3e9d023da4a36c
yellapantula-ramana <yramana123@gmail.com>
2024-07-28 16:08:09 +0530

    Updates

---
+++ precache-manifest.0be84aabd4dbb781f19cba046cc94c0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f80e48db096e019ea08d8cf1a4cff4b",
+    "url": "/yellapantula-ramana.github.io/spark-react-app.git/index.html"
+  },
+  {
+    "revision": "6157dd5c18d360737c6e",
+    "url": "/yellapantula-ramana.github.io/spark-react-app.git/static/css/main.79c22671.chunk.css"
+  },
+  {
+    "revision": "686197039a6a5d129d00",
+    "url": "/yellapantula-ramana.github.io/spark-react-app.git/static/js/2.e67a7231.chunk.js"
+  },
+  {
+    "revision": "6157dd5c18d360737c6e",
+    "url": "/yellapantula-ramana.github.io/spark-react-app.git/static/js/main.0b775983.chunk.js"
+  },
+  {
+    "revision": "06743cb1a1df38321fac",
+    "url": "/yellapantula-ramana.github.io/spark-react-app.git/static/js/runtime~main.0d387fdf.js"
+  }
+]);
